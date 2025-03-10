a219100a3b4c238f92b3ce036daa8ce2d2487b1b
Alice Smith <alice@webage.com>
2025-03-10 11:37:54 -0400

    server setup

---
+++ server/src/app.js
@@ -0,0 +1,14 @@
+import express, { json } from "express";
+
+// import quackRoutes from "./routes/quackRoutes.js";
+// import userRoutes from "./routes/userRoutes.js";
+import cors from "cors";
+
+const app = express();
+app.use(json());
+app.use(cors());
+
+// app.use("/api/quacks", quackRoutes);
+// app.use("/api/user", userRoutes);
+
+export default app;
