7ca2c2332a28b4c04203dfac340b3d8aa308c3c2
Dev Patel <devpdevp25@gmail.com>
2025-03-10 14:11:45 -0400

    added film from character

--- server/routes/charactersRoutes.js
+++ server/routes/charactersRoutes.js
@@ -4,5 +4,6 @@ import { getCharacterById } from "../controllers/characterController.js";
 const router = express.Router();
 
 router.get("/:id", getCharacterById);
+router.get("/:id/films", getFilmsFromCharacter);
 
 export default router;
