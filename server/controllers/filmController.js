b7210618b486a5c86eb67f3beb9c80c686af0d2a
Tia Nguyen <thuydung244112@gmail.com>
2025-03-10 14:58:34 -0400

    Get Planet from Film

--- server/controllers/filmController.js
+++ server/controllers/filmController.js
@@ -1,5 +1,6 @@
 import Film from "../models/filmModel.js";
 import Character from "../models/characterModel.js";
+import FilmsPlanet from "../models/filmsPlanetsModel.js";
 import FilmsCharacter from "../models/filmsCharactersModel.js";
 
 export const getFilmById = async (req, res) => {
@@ -26,19 +27,6 @@ export const getAllFilms = async (req, res) => {
   }
 };
 
-// export const getFilmCharacters = async (req, res) => {
-//   try {
-//     const film = await Film.findOne({ id: req.params.id });
-//     if (!film) {
-//       return res.status(404).json({ message: "Film not found" });
-//     }
-
-//     const characters = await Character.find({id: { $in: film.characters}});
-//     res.status(200).json(film);
-//   } catch (err) {
-//     res.status(500).json({ message: "Error fetching film", err });
-//   }
-// };
 
 export const getCharacterFromFilm= async (req, res) => {
   try {
@@ -53,5 +41,19 @@ export const getCharacterFromFilm= async (req, res) => {
     res
       .status(500)
       .json({ message: "Error fetching from films_characters", err });
+  }
+};
+
+export const getPlanetFromFilm = async (req, res) => {
+  try {
+    const filmsPlanet = await FilmsPlanet.find({
+      film_id: req.params.id,
+    });
+    if (!filmsPlanet) {
+      return res.status(404).json({ message: "FilmsPlanet not found" });
+    }
+    res.status(200).json(filmsPlanet);
+  } catch (err) {
+    res.status(500).json({ message: "Error fetching from films_planets", err });
   }
 };
--- server/routes/filmsRoutes.js
+++ server/routes/filmsRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getFilmById, getAllFilms, getCharacterFromFilm } from "../controllers/filmController.js";
+import { getFilmById, getAllFilms, getCharacterFromFilm, getPlanetFromFilm } from "../controllers/filmController.js";
 
 const router = express.Router();
 
 router.get("/:id", getFilmById);
 router.get("/", getAllFilms);
 router.get("/:id/characters", getCharacterFromFilm);
+router.get("/:id/planets", getPlanetFromFilm);
 export default router;
