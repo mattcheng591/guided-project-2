dfcf460a2bdc0cf09f7fdb534b93aed607b7f49e
Dev Patel <devpdevp25@gmail.com>
2025-03-11 10:51:11 -0400

    additional css

--- client/src/components/CharacterList.jsx
+++ client/src/components/CharacterList.jsx
@@ -3,13 +3,15 @@ import SingleCharacter from "./SingleCharacter";
 function Character(props) {
   const characterList = props.characters.map((character) => (
     <div className="col-md-3" key={character.id}>
-      <SingleCharacter name={character.name} id={character.id}  />
+      <SingleCharacter name={character.name} id={character.id} />
     </div>
   ));
 
   return (
     <div className="container">
-      <div className="row">{characterList}</div>
+      <h1 className="text-center">Star Wars Universe Lookup</h1>
+      <hr />
+      <div className="row justify-content-center">{characterList}</div>
     </div>
   );
 }
